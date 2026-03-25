a1143b2790b9927e8f530a960a15f7695cc07b73
mrGrANTt <sambistyyy0@gmail.com>
2026-03-25 16:28:52 +1000

    Angle counter

--- src/main/java/mrg/speedometer/util/ConfigValues.java
+++ src/main/java/mrg/speedometer/util/ConfigValues.java
@@ -6,24 +6,62 @@ import me.shedaniel.autoconfig.annotation.Config;
 import me.shedaniel.autoconfig.annotation.ConfigEntry;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import mrg.speedometer.Speedometer;
+import net.minecraft.client.MinecraftClient;
 
 @Config(name = Speedometer.MOD_ID)
 public class ConfigValues implements ConfigData {
     @ConfigEntry.Gui.Excluded
     public static ConfigValues INSTANCE;
 
+    public static final boolean ENABLED = true;
+    public static boolean ENABLED_SPEEDOMETER = true;
+    public static boolean ENABLED_ANGLE = true;
+    public static final int DILAY = 5;
+    public static final int SPEED_X = 10;
+    public static final int SPEED_Y = 15;
+    public static final int YAW_X = 10;
+    public static final int YAW_Y = 40;
+    public static final int PITCH_X = 10;
+    public static final int PITCH_Y = 50;
+    public static final float SCALE = 1;
+    public static final int COLOR = 0xFFFFFF;
+    public static final int COLOR_1 = 0xFF9500;
+    public static final int COLOR_2 = 0xFF2F00;
+
     public static void init() {
         AutoConfig.register(ConfigValues.class, GsonConfigSerializer::new);
         INSTANCE = AutoConfig.getConfigHolder(ConfigValues.class).getConfig();
     }
 
-    public boolean enabled = true;
+    public ConfigValues() {
+         this.enabled = ENABLED;
+         this.enabledSpeedometer = ENABLED_SPEEDOMETER;
+         this.enabledAngle = ENABLED_ANGLE;
+         this.dilay = DILAY;
+         this.speedX = SPEED_X;
+         this.speedY = SPEED_Y;
+         this.yawX = YAW_X;
+         this.yawY = YAW_Y;
+         this.pitchX = PITCH_X;
+         this.pitchY = PITCH_Y;
+         this.scale = SCALE;
+         this.color = COLOR;
+         this.color1 = COLOR_1;
+         this.color2 = COLOR_2;
+    }
 
-    public int dilay = 10;
-    public int x = 10;
-    public int y = 15;
-    public float scale = 1;
-    public int color = 0xFFFFFF;
-    public int color1 = 0xFF9500;
-    public int color2 = 0xFF2F00;
+    public boolean enabled;
+    public boolean enabledSpeedometer;
+    public boolean enabledAngle;
+    public int dilay;
+    public int speedX;
+    public int speedY;
+    public int yawX;
+    public int yawY;
+    public int pitchX;
+    public int pitchY;
+    public float scale;
+    public int color;
+    public int color1;
+    public int color2;
 }
